f7e02f900ba0e0b11821ea7a7dd5e3dfd9080c98
wildantisna <wildan27370@gmail.com>
2025-12-26 08:54:32 +0700

    feat: Add signup link and footer to login page

--- lib/feature/login/login_page.dart
+++ lib/feature/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ppob_koperasi_payment/feature/login/widgets/another_login.dart';
 import 'package:ppob_koperasi_payment/feature/login/widgets/form_login.dart';
+import 'package:ppob_koperasi_payment/feature/login/widgets/signup_from_login.dart';
+import 'package:ppob_koperasi_payment/utils/theme/app_color.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -27,10 +29,37 @@ class LoginPage extends StatelessWidget {
                 softWrap: true,
               ),
               const SizedBox(height: 24.0),
-              // The form login widget would be placed here
+              // Form Login
               const FormLogin(),
               const SizedBox(height: 24.0),
+              // Another Login
               const AnotherLogin(),
+              const SizedBox(height: 24.0),
+              const SignupFromLogin(),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0, top: 24.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Koperasi Sukasenang",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        "Jl. Anggrek Raya No. 17 RT 02/RW 05, Kel. Cipadung, Kec. Ujungberung, Kota Bandung, Prov. Jawa Barat",
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: AppColors.text70,
+                        ),
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
